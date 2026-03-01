0e450d09c4e8ef9c55e6951cac0f045d933c0dbd
Bhushan <alhatbhushan@gmail.com>
2026-03-01 16:35:59 +0530

    setup prettier

---
+++ tooling/prettier/index.js
@@ -0,0 +1,52 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+
+/** @type { PrettierConfig } */
+const config = {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+
+    plugins: [
+        '@ianvs/prettier-plugin-sort-imports',
+        'prettier-plugin-tailwindcss',
+    ],
+
+    tailwindFunctions: ['cn', 'cva'],
+    importOrder: [
+        '<TYPES>',
+        '^(react/(.*)$)|^(react$)|^(react-native(.*)$)',
+        '^(next/(.*)$)|^(next$)',
+        '^(expo(.*)$)|^(expo$)',
+        '<THIRD_PARTY_MODULES>',
+        '',
+        '<TYPES>^@acme',
+        '^@acme/(.*)$',
+        '',
+        '<TYPES>^[.|..|~]',
+        '^~/',
+        '^[../]',
+        '^[./]',
+    ],
+    importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+    importOrderTypeScriptVersion: '5.0.0',
+
+    overrides: [
+        {
+            files: '*.json.hbs',
+            options: {
+                parser: 'json',
+            },
+        },
+        {
+            files: '*.ts.hbs',
+            options: {
+                parser: 'babel',
+            },
+        },
+    ],
+};
+
+const foo = 'hello-world';
+
+export default config;
