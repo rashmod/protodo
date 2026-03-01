8b6db6f44b7c2199084056f3d0e934ab0391d664
Bhushan <alhatbhushan@gmail.com>
2026-03-01 23:26:50 +0530

    add a11y eslint plugin

--- tooling/eslint/react.ts
+++ tooling/eslint/react.ts
@@ -1,18 +1,26 @@
 import { defineConfig } from 'eslint/config'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactPlugin from 'eslint-plugin-react'
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
 
 export const reactConfig = defineConfig(
     {
         files: ['**/*.ts', '**/*.tsx'],
         ...reactPlugin.configs.flat.recommended,
         ...reactPlugin.configs.flat['jsx-runtime'],
+        plugins: {
+            'jsx-a11y': jsxA11yPlugin,
+        },
         languageOptions: {
             ...reactPlugin.configs.flat.recommended?.languageOptions,
             ...reactPlugin.configs.flat['jsx-runtime']?.languageOptions,
+            ...jsxA11yPlugin.flatConfigs.recommended.languageOptions,
             globals: {
                 React: 'writable',
             },
+        },
+        rules: {
+            ...jsxA11yPlugin.flatConfigs.recommended.rules,
         },
     },
     reactHooks.configs.flat['recommended-latest']!
